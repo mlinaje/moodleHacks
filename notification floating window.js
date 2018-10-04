c66088f9f5a774916c8ee3795866a512eac9a0c9
Marino Linaje <marino.linaje@gmail.com>
2018-10-04 09:47:00 +0200

    Add files via upload
    Notification floating window that can be used to include e.g., advice or any kind of text (HTML, including images, links,...)
    The window can be closed by the user.

---
+++ notification floating window.js
@@ -0,0 +1,3 @@
+<div id="tutoriasFlotantes" style="position: fixed; top: 45px; right: 0px;padding:5px; background-color: rgb(0, 68, 0);">
+<a id="linkCerrar" title="cerrar tutorías" style="color: #999999" href="#" onclick="Y.one('#tutoriasFlotantes').hide();return false;">[x]</a> <span style="color: #FFFFFF">¿Tienes dudas?</span> <br>
+<a style="color: #00FF00" href="http://marinolinaje.youcanbook.me" target="_blank">¡reserva tutorías online!</a></div>
