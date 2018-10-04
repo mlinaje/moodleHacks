0d61b207eb2af15ed614c73b0705b4846e93c422
Marino Linaje <marino.linaje@gmail.com>
2018-10-04 09:48:40 +0200

    Add files via upload
    An example of gamification of moodle questionaries. The heart icons can be adapted to you needs, as well as the size of the icons.

---
+++ lifes - gamification script for moodle.js
@@ -0,0 +1,5 @@
+<p>Tienes 3 vidas&nbsp;(arriba a la derecha) así que tienes 3 intentos y nos quedamos con tu mejor puntuación ;-)</p><p>Entre intento e intento hay un retardo de 5 minutos, para animarte a volverte a ver lo que no te haya quedado claro en los vídeos y hacerlo mucho mejor</p>
+<div id="vidasFlotantes" style="position:fixed; top:45px; right:0px; padding:5px; background-color:rgb(0, 68, 0); opacity:1;text-align:center;"><span style="color: #FFFFFF">mis vidas</span> <br>
+<span id="vidas" style="font-size:x-large;"></span>
+</div>
+<script> function pintaVidas(){ var intentosTotales = Y.one('.quizinfo >p').get('text').slice(-1); var intentosRealizados = Y.all('.quizattemptsummary >tbody >tr').size(); var cadenaMisVidas=""; for (var i=0;i<intentosTotales-intentosRealizados;i++) cadenaMisVidas+="&#x1F49A;"; for (i;i<intentosTotales;i++)	 cadenaMisVidas+="&#x1F494;"; Y.one('#vidas').setContent(cadenaMisVidas); }; YUI().use('event-base', function (Y) { 	Y.on('domready', function () { pintaVidas(); }); }); </script>
